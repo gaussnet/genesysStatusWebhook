ceee666aca463138d214752efeec3a192d01a194
Juan Manuel Urdinola <L0651427@bancogalicia.com.ar>
2026-06-08 14:00:03 -0300

    se modifica main

--- main.py
+++ main.py
@@ -4,25 +4,6 @@ import os
 
 app = FastAPI()
 
-# estados considerados WARNING+
-COMPONENT_ALERT_STATES = [
-    "degraded_performance",
-    "partial_outage",
-    "major_outage"
-]
-
-INCIDENT_ALERT_IMPACT = [
-    "minor",
-    "major",
-    "critical"
-]
-
-#TARGET_REGION = "Americas (US East)"
-#TARGET_REGION = "Americas (Sao Paulo)"
-TARGET_REGION = "EMEA (UAE)"
-#TARGET_REGION = "EMEA (London)"
-
-
 @app.post("/webhook/genesys-status")
 async def genesys_status_webhook(request: Request):
     payload = await request.json()
@@ -30,3 +11,8 @@ async def genesys_status_webhook(request: Request):
     print("Received payload:", payload)
 
     return {"message": "Payload recibido correctamente"}
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
